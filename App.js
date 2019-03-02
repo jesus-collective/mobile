c4c9043fcb1c9b575aeeb8e77724b11183a9da5c
George Bell <georgebell@GeorgeBellsMBP.home>
2019-03-02 15:43:50 -0500

    Add initial code

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Amplify from 'aws-amplify';
+import awsConfig from './src/aws-exports';
+
+Amplify.configure(awsConfig);
+
+import { withAuthenticator } from 'aws-amplify-react-native';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+export default withAuthenticator(App);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
