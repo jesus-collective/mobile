f08a665bad792847fcc83225d439bcadd4c3ea3f
George Bell <georgebell@Georges-MacBook-Pro.local>
2020-02-21 20:03:21 -0800

    Circle status

--- components/CourseSidebar/CourseSidebar.tsx
+++ components/CourseSidebar/CourseSidebar.tsx
@@ -4,6 +4,7 @@ import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
 import { Image, Text } from 'react-native';
 import styles from './style.js'
+import AnimatedProgressWheel from 'react-native-progress-wheel';
 
 interface Props {
     navigation: any,
@@ -38,15 +39,37 @@ class CourseSidebar extends React.Component<Props, State> {
         //const { navigate } = this.props.navigation;
         return (
             <Container style={{ flex: 15, backgroundColor: "#000000" }}>
-                <Button style={{marginBottom:40}}
+                <Button style={{ marginBottom: 40 }}
                     transparent
                     onPress={this.openHome}>
                     <Image style={styles.logo}
                         source={require('../../components/Header/icon.png')}
                     /></Button>
-                <Button transparent onPress={this.openCourseHome}><Image style={{ marginLeft:20,width: "22px", height: "22px" }} source={require('../../assets/svg/home.svg')} /><Text style={styles.courseSidebarFontRegular}>Home</Text></Button>
-                <Button transparent onPress={this.openCourseDetails}><Image style={{ marginLeft:20,width: "22px", height: "22px" }} source={require('../../assets/svg/education.svg')} /><Text style={styles.courseSidebarFontRegular}>Course</Text></Button>
-                <Button transparent onPress={this.openCourseCoaching}><Image style={{ marginLeft:20,width: "22px", height: "22px" }} source={require('../../assets/svg/calendar.svg')} /><Text style={styles.courseSidebarFontRegular}>Coaching</Text></Button>
+                <Button transparent onPress={this.openCourseHome}><Image style={{ marginLeft: 20, width: "22px", height: "22px" }} source={require('../../assets/svg/home.svg')} /><Text style={styles.courseSidebarFontRegular}>Home</Text></Button>
+                <Button transparent onPress={this.openCourseDetails}><Image style={{ marginLeft: 20, width: "22px", height: "22px" }} source={require('../../assets/svg/education.svg')} /><Text style={styles.courseSidebarFontRegular}>Course</Text></Button>
+                <Button transparent onPress={this.openCourseCoaching}><Image style={{ marginLeft: 20, width: "22px", height: "22px" }} source={require('../../assets/svg/calendar.svg')} /><Text style={styles.courseSidebarFontRegular}>Coaching</Text></Button>
+                <Container style={{backgroundColor:"#00000000"}}>
+                    <AnimatedProgressWheel
+                        size={120}
+                        width={10}
+                        color={'#71C209'}
+                        progress={45}
+                        backgroundColor={'#333333'}
+                        animateFromValue={0}
+                        duration={1000}
+                    />
+                    <Container style={{ backgroundColor:"#00000000",position: "absolute", top: 15, left: 15 }}>
+                        <AnimatedProgressWheel
+                            size={90}
+                            width={10}
+                            color={'#F0493E'}
+                            progress={45}
+                            backgroundColor={'#333333'}
+                            animateFromValue={0}
+                            duration={1000}
+                        />
+                    </Container>
+                </Container>
             </Container>
         )
     }
