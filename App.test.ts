c6b0034972a586511d5ca516743a5ac6dfc4ed3f
George Bell <george.bell@themeetinghouse.com>
2020-04-27 17:50:43 -0400

    Add testing capabilities

---
+++ App.test.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+
+import AwesomeApp from './App';
+
+describe('<AwesomeApp />', () => {
+    it('has 1 child', () => {
+       
+    });
+});
