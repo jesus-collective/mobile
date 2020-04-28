87c1ea11a1ddca31747430e282e35fc52d9167ec
George Bell <george.bell@themeetinghouse.com>
2020-04-28 00:18:33 -0400

    Some updates for coverage and testing

--- App.test.tsx
+++ App.test.tsx
@@ -6,7 +6,7 @@ import AwesomeApp from './App'
 import MutationObserver from '@sheerun/mutationobserver-shim'
 window.MutationObserver = MutationObserver
 
-jest.mock('axios')
+
 
 test('loads and displays login', async () => {
     const { getByText, findByText } = render(
@@ -15,4 +15,25 @@ test('loads and displays login', async () => {
     const items = await findByText("SIGN IN")
     fireEvent.click(getByText('SIGN IN'))
     expect(screen.getByText('SIGN IN')).toHaveAttribute('style')
+})
+test('loads and displays create account', async () => {
+    const { getByText, findByText } = render(
+        <AwesomeApp navigation={null} onStateChange={() => { }} />
+    )
+    const items = await findByText("Create an Account")
+    fireEvent.click(getByText('Create an Account'))
+    const items2 = await findByText("Create a new account")
+    expect(screen.getByText('Create a new account')).toHaveAttribute('style')
+  
+})
+
+test('loads and displays forgot password', async () => {
+    const { getByText, findByText } = render(
+        <AwesomeApp navigation={null} onStateChange={() => { }} />
+    )
+    const items = await findByText("Forgot password?")
+    fireEvent.click(getByText('Forgot password?'))
+    const items2 = await findByText("Reset your password")
+    expect(screen.getByText('Reset your password')).toHaveAttribute('style')
+  
 })
