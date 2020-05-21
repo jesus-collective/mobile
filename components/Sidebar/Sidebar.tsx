3da3d6e3c633596c3eaf7ef841ae4b314e147fac
George Bell <47749730+GeorgeBellTMH@users.noreply.github.com>
2020-05-21 00:01:49 -0400

    Mobile sidebar has correct items, and they work

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,61 +1,29 @@
 import * as React from 'react';
 import { Container, Content, List, ListItem, Header } from "native-base";
 import { Text } from 'react-native'
-
+//import useNavigation from 'react-navigation';
+import { withNavigation } from 'react-navigation';
 const routes = [
   {
     name: "Home",
     route: "HomeScreen"
   },
   {
-    name: "Explore",
-    route: "ExploreScreen"
-  },
-  {
-    name: "Support",
-    route: "SupportScreen"
-  },
-  {
-    name: "Get Involved",
-    route: "GetInvolvedScreen"
-  },
-  {
-    name: "Contact",
-    route: "ContactScreen"
-  },
-  {
-    name: "Kids and Youth",
-    route: "KidsAndYouthScreen"
-  },
-
-  {
     name: "Events",
-    route: "NewsScreen"
+    route: "EventsScreen"
   },
   {
     name: "Groups",
-    route: "NewsScreen"
-  },
-  {
-    name: "Training",
-    route: "NewsScreen"
+    route: "GroupsScreen"
   },
   {
     name: "Resources",
-    route: "NewsScreen"
+    route: "ResourcesScreen"
   },
 
   {
-    name: "Profile",
-    route: "ProfileScreen"
-  },
-  {
-    name: "Group",
-    route: "GroupScreen"
-  },
-  {
-    name: "Event",
-    route: "EventScreen"
+    name: "Courses",
+    route: "CoursesScreen"
   }
 
 ];
@@ -65,29 +33,33 @@ interface Props {
 interface State {
 
 }
-export default class SideBar extends React.Component<Props, State> {
+class SideBar extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+  }
 
   render() {
     return (
       <Container style={{ width: "100%" }}>
         <Content>
-        <Header style={{ backgroundColor: "#FFFFFF" }}></Header>
-        <List
-          dataArray={routes}
-          keyExtractor={data => data.name}
-          renderRow={data => {
-            return (
-              <ListItem
-                style={{ marginRight: 20 }}
-                button
-                onPress={() => this.props.navigation.push(data.route)}>
-                <Text style={{ fontFamily: "Graphik-Regular-App", fontSize: 18, lineHeight: 30, color: "#333333" }}>{data.name}</Text>
-              </ListItem>
-            );
-          }}
-        />
-      </Content>
+          <Header style={{ backgroundColor: "#FFFFFF" }}></Header>
+          <List
+            dataArray={routes}
+            keyExtractor={data => data.name}
+            renderRow={data => {
+              return (
+                <ListItem
+                  style={{ marginRight: 20 }}
+                  button
+                  onPress={() => { this.props.navigation.navigate(data.route) }}>
+                  <Text style={{ fontFamily: "Graphik-Regular-App", fontSize: 18, lineHeight: 30, color: "#333333" }}>{data.name}</Text>
+                </ListItem>
+              );
+            }}
+          />
+        </Content>
       </Container>
     )
   }
 }
+export default withNavigation(SideBar)
