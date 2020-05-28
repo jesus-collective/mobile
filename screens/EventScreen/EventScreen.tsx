9f5ebaf69149c20a942946b0b9cdeec49f623e46
Mateus <msmmacedo@gmail.com>
2020-05-28 19:22:38 -0400

    desktop - events page detail - left card drop down event fix height/width fix

--- screens/EventScreen/EventScreen.tsx
+++ screens/EventScreen/EventScreen.tsx
@@ -300,7 +300,7 @@ export default class EventScreen extends React.Component<Props, State>{
                   {this.state.isEditable ? <Picker
                     mode="dropdown"
                     iosIcon={<Icon name="arrow-down" />}
-                    style={{ width: "75%", marginBottom: 30, marginTop: 30, fontSize: 16, height: 100, flexGrow: 0 }}
+                    style={{ width: "50%", marginBottom: 30, marginTop: 30, fontSize: 16, height: 30, flexGrow: 0 }}
                     placeholder="Event type"
                     placeholderStyle={{ color: "#bfc6ea" }}
                     placeholderIconColor="#007aff"
