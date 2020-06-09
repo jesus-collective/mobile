1d520695b761e257daa5f23f547c8bd1897040de
Jon Esparaz <jon.esparaz@gmail.com>
2020-06-09 01:24:19 -0400

    color props

--- components/JCSwitch/JCSwitch.tsx
+++ components/JCSwitch/JCSwitch.tsx
@@ -8,10 +8,16 @@ interface Props {
   initState: boolean
   onPress(): any
   containerWidth?: number
+  onColor?: string
+  offColor?: string
+  thumbColor?: string
 }
 interface State {
   enabled: boolean
   animationState: any
+  onColor: string
+  offColor: string
+  thumbColor: string
 }
 
 export default class JCSwitch extends JCComponent<Props, State> {
@@ -19,7 +25,10 @@ export default class JCSwitch extends JCComponent<Props, State> {
       super(props);
       this.state = {
         enabled: this.props.initState,
-        animationState: new Animated.Value(this.props.initState ? 1 : 0)
+        animationState: new Animated.Value(this.props.initState ? 1 : 0),
+        onColor: this.props.onColor ? this.props.onColor : '#333333',
+        offColor: this.props.offColor ? this.props.offColor : '#aaaaaa',
+        thumbColor: this.props.thumbColor ? this.props.thumbColor : '#ffffff'
       }
   }
 
@@ -38,13 +47,13 @@ export default class JCSwitch extends JCComponent<Props, State> {
       <Text style={this.styles.style.fontMyMapOptions}>{this.props.switchLabel}</Text>
       <TouchableWithoutFeedback onPress={() => this.onPress()}>
         <View style={{
-          backgroundColor: this.state.enabled ? '#333333' : '#aaaaaa',
-          borderColor: this.state.enabled ? '#333333' : '#aaaaaa',
+          backgroundColor: this.state.enabled ? this.state.onColor : this.state.offColor,
+          borderColor: this.state.enabled ? this.state.onColor : this.state.offColor,
           borderWidth: 2, borderRadius: 25, width: 50, height: 20
         }}>
           <Animated.View
             style={{
-              backgroundColor: '#ffffff', borderRadius: 25, width: 16, height: 16,
+              backgroundColor: this.state.thumbColor, borderRadius: 25, width: 16, height: 16,
               transform: [{
                 translateX: this.state.animationState.interpolate({
                   inputRange: [0, 1],
