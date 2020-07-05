a0e6dcfcce311d8f80e56c1833f31f8f791fc2fc
George Bell <47749730+GeorgeBellTMH@users.noreply.github.com>
2020-07-05 17:09:47 -0400

    Initial DM alert handler

---
+++ amplify/backend/function/jcProcessDMAlerts/src/index.js
@@ -0,0 +1,13 @@
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */exports.handler = event => {
+  //eslint-disable-line
+  console.log(JSON.stringify(event, null, 2));
+  event.Records.forEach(record => {
+    console.log(record.eventID);
+    console.log(record.eventName);
+    console.log('DynamoDB Record: %j', record.dynamodb);
+  });
+  return Promise.resolve('Successfully processed DynamoDB record');
+};
