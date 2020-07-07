c5a5e89addaf02599103b32e2589b9321ad321ab
Mateus <msmmacedo@gmail.com>
2020-07-06 22:56:33 -0400

    dashboard - courses -card image height fix

--- components/MyGroups/MyGroups.tsx
+++ components/MyGroups/MyGroups.tsx
@@ -531,7 +531,7 @@ export default class MyGroups extends JCComponent<Props, State> {
   renderCourse(item: any): React.ReactNode {
     return <Card style={[this.styles.style.courseCard, { width: this.state.cardWidth }]}>
       <CardItem bordered style={{ paddingLeft: 0, paddingRight: 0, paddingTop: 0, paddingBottom: 0, width: this.state.cardWidth, right: 5 }}>
-        <Image style={{ margin: 0, padding: 0, width: this.state.cardWidth, height: 20 }} source={require('../../assets/svg/pattern.svg')}></Image>
+        <Image style={{ margin: 0, padding: 0, width: this.state.cardWidth, height: 70 }} source={require('../../assets/svg/pattern.svg')}></Image>
       </CardItem>
       <CardItem ><Text ellipsizeMode='tail' numberOfLines={3} style={this.styles.style.fontTitleGroup}>{item.name}</Text></CardItem>
       <CardItem ><Text ellipsizeMode='tail' numberOfLines={1} style={this.styles.style.fontDetail}>Last Updated: {item.lastupdated}</Text></CardItem>
