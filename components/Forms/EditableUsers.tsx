3edbd94fc21b59dcb10be3ca7fe821ad9607c5a1
George Bell <47749730+GeorgeBellTMH@users.noreply.github.com>
2020-07-08 02:11:28 -0400

    Pull editableusers from ElasticSearch

--- components/Forms/EditableUsers.tsx
+++ components/Forms/EditableUsers.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Chips, { Chip } from 'react-chips'
-
+import * as queries from '../../src/graphql/queries';
+import { API } from 'aws-amplify';
+import GRAPHQL_AUTH_MODE from 'aws-amplify-react-native'
 import { Text } from 'react-native'
 import JCComponent, { JCState } from '../JCComponent/JCComponent';
 import ProfileImage from '../../components/ProfileImage/ProfileImage'
@@ -49,12 +51,29 @@ export default class EditableText extends JCComponent<Props> {
         this.props.onChange(val)
     }
     async autoCompleteUser(value: string): Promise<any[]> {
-        //TODO: Get users from DB
-        return [
-            { name: "George", id: "000" },
-            { name: "Dave", id: "001" },
-            { name: "Jon", id: "002" }
-        ]
+        let searchUsers: any
+        try {
+            searchUsers = await API.graphql({
+                query: queries.searchUsers,
+                variables: {
+                    filter: {
+                        or: [
+                            { given_name: { wildcard: value.toLowerCase() + "*" } },
+                            { family_name: { wildcard: value.toLowerCase() + "*" } }
+                        ]
+                    },
+                    limit: 10
+                },
+                authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS
+            })
+            console.log({ searchUsers: searchUsers })
+            return searchUsers.data.searchUsers.items
+        }
+        catch (e) {
+            console.log({ error: e })
+            return e.data.searchUsers.items
+        }
+
     }
     render(): React.ReactNode {
 
@@ -70,8 +89,8 @@ export default class EditableText extends JCComponent<Props> {
                         return (
                             <Chip>
                                 <span>
-                                    {this.props.showProfileImages ? <ProfileImage size="xsmall" user={value.id}></ProfileImage> : null}
-                                    <Text>{value.name}</Text>
+                                    {this.props.showProfileImages ? <ProfileImage size="xsmall" user={value}></ProfileImage> : null}
+                                    <Text>{value.given_name} {value.family_name}</Text>
                                 </span>
                             </Chip>
                         )
@@ -88,8 +107,8 @@ export default class EditableText extends JCComponent<Props> {
                     renderSuggestion={(value) => {
                         return (
                             <Text>
-                                {this.props.showProfileImages ? <ProfileImage size="xsmall" user={value.id}></ProfileImage> : null}
-                                {value.name}
+                                {this.props.showProfileImages ? <ProfileImage size="xsmall" user={value}></ProfileImage> : null}
+                                {value.given_name} {value.family_name}
                             </Text>
                         )
                     }}
@@ -98,8 +117,8 @@ export default class EditableText extends JCComponent<Props> {
         else
             return <div>{
                 this.props.value.map((item) => {
-                    { this.props.showProfileImages ? <ProfileImage size="small" user={item.id}></ProfileImage> : null }
-                    <Text style={this.props.textStyle}>{item.name}</Text>
+                    { this.props.showProfileImages ? <ProfileImage size="small" user={item}></ProfileImage> : null }
+                    <Text style={this.props.textStyle}>{item.given_name} {item.family_name}</Text>
                 })
             }
             </div>
---
+++ python/migrateToEs.sh
@@ -0,0 +1,5 @@
+python3 ddb_to_ess.py \
+  --rn 'us-east-1'  \
+  --tn 'User-2ht5eyf3rfct7f3slbiz6sme5u-dev'  \
+  --lf 'arn:aws:lambda:us-east-1:119330689042:function:DdbToEsFn-2ht5eyf3rfct7f3slbiz6sme5u-dev'  \
+  --esarn 'arn:aws:dynamodb:us-east-1:119330689042:table/User-2ht5eyf3rfct7f3slbiz6sme5u-dev/stream/2019-20-03T00:00:00.350' 
