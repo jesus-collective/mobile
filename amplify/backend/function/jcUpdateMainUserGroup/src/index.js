7c6e11af81600f698a61ec9483b3b811b7ca195d
George Bell <47749730+GeorgeBellTMH@users.noreply.github.com>
2020-07-16 20:04:10 -0400

    For now everyone is a partner

--- amplify/backend/function/jcUpdateMainUserGroup/src/index.js
+++ amplify/backend/function/jcUpdateMainUserGroup/src/index.js
@@ -206,10 +206,10 @@ query ListUsers(
       //Everyone else ->UnVerified
       var json2 = await Amplify.API.graphql({
         query: updateUser,
-        variables: { input: { id: item.id, mainUserGroup: "Verified" } },
+        variables: { input: { id: item.id, mainUserGroup: "Partner" } },
         authMode: "AMAZON_COGNITO_USER_POOLS"
       });
-      console.log({ Updated: { id: item.id, mainUserGroup: "Verified" } })
+      console.log({ Updated: { id: item.id, mainUserGroup: "Partner" } })
     }))
     console.log("Done");
     return {
