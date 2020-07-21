a790152ade29d9c8bcde660dbb4d679e1653d1f0
George Bell <47749730+GeorgeBellTMH@users.noreply.github.com>
2020-07-21 16:28:25 -0400

    Index changes

--- python/fix_null_values.py
+++ python/fix_null_values.py
@@ -10,10 +10,14 @@ def main():
     parser.add_argument('--default', required=True)
     parser.add_argument(
         '--type', help='type of default as in the boto3 spec (e.g. \'S\' for string)', required=True)
-    parser.add_argument('--ak', help='aws access key', required=True)
-    parser.add_argument('--sk', help='aws secret key', required=True)
+    parser.add_argument('--ak', help='aws access key', required=False)
+    parser.add_argument('--sk', help='aws secret key', required=False)
     parser.add_argument('--rn', help='aws region', required=True)
     args = parser.parse_args()
+    if (args.ak is None or args.sk is None):
+      credentials = boto3.Session(profile_name="amplify").get_credentials()
+      args.sk = args.sk or credentials.secret_key
+      args.ak = args.ak or credentials.access_key
 
     client = boto3.client('dynamodb', region_name=args.rn,
                           aws_access_key_id=args.ak, aws_secret_access_key=args.sk)
@@ -25,26 +29,24 @@ def do_stuff(table, col, default, default_type):
     response = client.scan(
         TableName=table,
         ExpressionAttributeNames={
-            'ID': 'id'
+            '#ID': 'id'
         },
         ProjectionExpression='#ID',
     )
 
     items = response['Items']
-
     for item in items:
+        print (item)
         update = client.update_item(
             TableName=table,
             Key=item,
-            ExpressionAttributeNames={
-                'ID': 'id'
-            },
+           
             ExpressionAttributeValues={
                 ':default': {default_type: default},
                 ':isNull': {'NULL': True}
             },
-            UpdateExpression='SET {col} = :default',
-            ConditionExpression='attribute_not_exists({col}) OR {col} = :isNull',
+            UpdateExpression="SET "+col+" = :default",
+            ConditionExpression="attribute_not_exists("+col+") OR "+col+" = :isNull",
         )
         print(update)
 
