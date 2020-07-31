64563b9bfbd12171d4ca7eab693e37f5f6c46445
George Bell <47749730+GeorgeBellTMH@users.noreply.github.com>
2020-07-31 00:02:00 -0400

    Introduction should be welcome message

--- components/Forms/EditableRichText.tsx
+++ components/Forms/EditableRichText.tsx
@@ -120,7 +120,9 @@ export default class EditableRichText extends JCComponent<Props, State> {
             else
                 return <TouchableOpacity onPress={() => { this.setState({ isEditMode: true }) }}>
                     <div id="comment-div">
-                        <div dangerouslySetInnerHTML={{ __html: this.convertCommentFromJSONToHTML(this.props.value) }} style={{ fontFamily: 'Graphik-Regular-App', fontSize: 16, lineHeight: 26, color: "#333333", marginTop: 0, paddingTop: 0, height: 50 }}></div>
+                        <div
+                            dangerouslySetInnerHTML={{ __html: this.convertCommentFromJSONToHTML(this.props.value) }}
+                            style={{ fontFamily: 'Graphik-Regular-App', fontSize: 16, lineHeight: 26, color: "#333333", marginTop: 0, paddingTop: 0, display: "inline" }}></div>
                         <div style={{ fontFamily: 'Graphik-Bold-App', fontWeight: 'bold', fontSize: 16, marginTop: 0, color: '#F0493E', textDecoration: 'underline' }}>Hold to Edit</div>
                     </div>
                 </TouchableOpacity>
