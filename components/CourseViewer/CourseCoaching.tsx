c9ff9453d233e2878490e3292fab099360d81f1e
Mateus <msmmacedo@gmail.com>
2020-08-10 22:41:48 -0400

    leadership dev - course coaching - card fix

--- components/CourseViewer/CourseCoaching.tsx
+++ components/CourseViewer/CourseCoaching.tsx
@@ -1,5 +1,5 @@
 ﻿import React from 'react';
-import { Icon, Picker, StyleProvider, Container, Content } from 'native-base';
+import { Icon, Picker, StyleProvider, Container, Content, Card } from 'native-base';
 import { Text } from 'react-native'
 import JCButton, { ButtonTypes } from '../Forms/JCButton'
 
@@ -41,29 +41,30 @@ class CourseCoachingImpl extends JCComponent<Props>{
                   <Container style={{ flex: 80 }}>
                     <Content style={{ flex: 80 }}>
                       <Container style={{ display: "flex", flexDirection: "row", justifyContent: 'flex-start' }}>
-                        <Container style={{ flex: 100, flexDirection: "column", justifyContent: 'flex-start', paddingLeft: '5%' }}>
-                          <Container style={{ flexDirection: 'row', paddingBottom: 30, paddingTop: 30 }}>  
+                        <Container style={{ flex: 70, flexDirection: "column", justifyContent: 'flex-start', paddingLeft: '5%', marginRight: 30 }}>
+                          <Container style={{ flexDirection: 'row', paddingBottom: 30, paddingTop: 30, marginBottom: 250 }}>  
                             <Container style={{ flexDirection: 'column', marginTop: 30, flex: 20 }}>
                               <Image style={{ margin: 0, padding: 0, width: 110, height: 136, marginBottom: 20, marginLeft: '15%' }} source={require("../../assets/profile-placeholder.png")} />
                               <JCButton buttonType={ButtonTypes.CourseHome} onPress={() => { null }}>Book a Call</JCButton>
                               <JCButton buttonType={ButtonTypes.CourseHome} onPress={() => { null }}>Send Message</JCButton>
                             </Container>
                             <Container style={{ flex: 80, height: 200 }}>
-                              <Text style={{ marginTop: 30, marginLeft: 30, marginRight: 30, fontFamily: 'Graphik-Regular-App', fontSize: 20, lineHeight: 30 }}>Hi </Text>
-                              <Text style={{ marginTop: 30, marginLeft: 30, marginRight: 30, fontFamily: 'Graphik-Regular-App', fontSize: 20, lineHeight: 30 }}>For your journey in leadership formation, I’d like to invite you to our bi-weekly coaching sessions where we can connect and discuss issues in more details. Let’s schedule our Coaching Calls - talk soon!
+                              <Text style={{ marginTop: 30, marginLeft: 60, marginRight: 30, fontFamily: 'Graphik-Regular-App', fontSize: 20, lineHeight: 30 }}>Hi </Text>
+                              <Text style={{ marginTop: 30, marginLeft: 60, marginRight: 30, fontFamily: 'Graphik-Regular-App', fontSize: 20, lineHeight: 30 }}>For your journey in leadership formation, I’d like to invite you to our bi-weekly coaching sessions where we can connect and discuss issues in more details. Let’s schedule our Coaching Calls - talk soon!
+
                               Thanks! - Jon</Text>
                             </Container>
                           </Container>
-                          <Container>
-                            <Text>Schedule your</Text>
-                            <Text>30 Minute Coaching Call</Text>
+                          <Card style={{ width: '100%', borderColor: '#FFFFFF', paddingLeft: 30, paddingRight: 30, boxShadow: '0 6px 20px 0 rgba(0, 0, 0, 0.19)', marginTop: 100, paddingTop: 30, paddingBottom: 30, height: 400 }}>
+                            <Text style={{ fontFamily: 'Graphik-Regular-App', fontSize: 16, lineHeight: 24 }}>Schedule your</Text>
+                            <Text style={{ fontFamily: 'Graphik-Bold-App', fontSize: 28, lineHeight: 33 }}>30 Minute Coaching Call</Text>
 
                             <Container style={{ flexDirection: "row" }}>
-                              <Container style={{ flex: 50 }}>
+                              <Container style={{ flex: 40, marginTop: 30, height: 50, marginRight: 30 }}>
                                 <Picker
                                   mode="dropdown"
                                   iosIcon={<Icon name="arrow-down" />}
-                                  style={{ width: undefined }}
+                                  style={{ width: '100%', height: 50, paddingLeft: 15, paddingRight: 15 }}
                                   placeholder="Select your Coach"
                                   placeholderStyle={{ color: "#bfc6ea" }}
                                   placeholderIconColor="#007aff"
@@ -77,15 +78,17 @@ class CourseCoachingImpl extends JCComponent<Props>{
                                 </Picker>
 
                               </Container>
-                              <Container style={{ flex: 50 }}>
-                                <Text>Please confirm</Text>
-                                <Text>you’re going to schedule coaching call with Jon Hand.</Text>
-                                <Text>30 minutes</Text>
-                                <Text>Monday, August 23  -  1:30 PM – 2:00 PM</Text>
-                                <JCButton buttonType={ButtonTypes.Outline} onPress={() => { null }}>Yes, schedule call</JCButton>
+                              <Container style={{ flex: 60, marginTop: 30, height: 'auto', alignContent: 'space-between' }}>
+                                <Container>
+                                  <Text style={{ fontFamily: 'Graphik-Regular-App', fontSize: 28, lineHeight: 33 }}>Please confirm</Text>
+                                  <Text style={{ fontFamily: 'Graphik-Regular-App', fontSize: 16, lineHeight: 24 }}>you’re going to schedule coaching call with Jon Hand.</Text>
+                                  <Text style={{ fontFamily: 'Graphik-Regular-App', fontSize: 14, lineHeight: 24, marginTop: 35 }}>30 minutes</Text>
+                                  <Text style={{ fontFamily: 'Graphik-Regular-App', fontSize: 14, lineHeight: 24 }}>Monday, August 23  -  1:30 PM – 2:00 PM</Text>
+                                </Container>
+                                <JCButton buttonType={ButtonTypes.CourseHome} onPress={() => { null }}>Yes, schedule call</JCButton>
                               </Container>
                             </Container>
-                          </Container>
+                          </Card>
                         </Container>
                         <Container style={{ flex: 30, flexDirection: "column", alignContent: 'flex-start', alignItems: 'flex-start', justifyContent: 'flex-start' }}>
 
