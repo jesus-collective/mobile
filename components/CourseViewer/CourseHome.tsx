1d737e499cbc35714cd26aba029b055bdfa75ee2
Mateus <msmmacedo@gmail.com>
2020-08-14 12:10:54 -0400

    leadership dev - course home - message board margin right fix

--- components/CourseViewer/CourseHome.tsx
+++ components/CourseViewer/CourseHome.tsx
@@ -90,7 +90,7 @@ class CourseHomeImpl extends JCComponent<Props>{
                               <JCButton onPress={() => { null }} buttonType={ButtonTypes.CourseHome}>Book a Call</JCButton>
                               <JCButton onPress={() => { this.openConversation(state.instructor?.id, state.instructor?.given_name + " " + state.instructor?.family_name) }} buttonType={ButtonTypes.TransparentBoldOrange}>Send Message</JCButton>
                             </Container>
-                            <Container style={{ flex: 80, height: 200 }}>
+                            <Container style={{ flex: 80, height: 200, marginRight: 50 }}>
                               <Text style={{ marginTop: 30, marginLeft: 30, marginRight: 30, fontFamily: 'Graphik-Regular-App', fontSize: 20, lineHeight: 30 }}>
                                 {state.courseData ?
                                   <EditableRichText onChange={(val) => { actions.updateCourse("introduction", val) }}
