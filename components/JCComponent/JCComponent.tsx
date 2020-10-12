a177f6816c65cd86c388acadf62dfa261f094a7c
George Bell <47749730+GeorgeBellTMH@users.noreply.github.com>
2020-10-11 22:42:52 -0400

    Clean up unhandled promise in jccomponent

--- components/JCComponent/JCComponent.tsx
+++ components/JCComponent/JCComponent.tsx
@@ -6,42 +6,47 @@ import { Auth } from 'aws-amplify';
 
 export interface JCState {
     groups: any
+    groupsLoaded: boolean
 }
 export default class JCComponent<Props = any, State extends JCState = any> extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props)
         this.state = this.getInitialState()
         try {
-            const user = Auth.currentAuthenticatedUser()
-            user.then((user) => {
-                this.setState({
-                    groups: user.signInUserSession.accessToken.payload["cognito:groups"]
-                })
-            })
+            this.updateGroups()
         }
         catch (e) {
             console.log(e)
         }
     }
     componentDidMount(): void {
-
         Dimensions.addEventListener('change', () => { this.styles.updateStyles(this) })
     }
     protected getInitialState(): State {
         return { groups: null } as JCState as State;
     }
-
+    updateGroups(): void {
+        const user = Auth.currentAuthenticatedUser()
+        user.then((user) => {
+            this.setState({
+                groups: user.signInUserSession.accessToken.payload["cognito:groups"],
+                groupsLoaded: true
+            })
+        }).catch((e) => {
+            this.setState({
+                groupsLoaded: false
+            })
+        })
+    }
     isMemberOf(group: string): boolean {
         if (this.state.groups)
             return this.state.groups.includes(group)
-        else return false
-
+        else
+            return false
     }
     groups = null
     styles = MainStyles.getInstance();
-
     componentWillUnmount(): void {
         Dimensions.removeEventListener("change", () => { this.styles.updateStyles(this) });
     }
-
 }
