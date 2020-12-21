a532332efc93388117781e341738ddb05bed227a
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2020-12-21 16:46:38 -0500

    bootstrap lambdas for inviting users to join orgs

---
+++ amplify/backend/function/jcmobileSendInviteToOrgUser/src/index.js
@@ -0,0 +1,24 @@
+/* Amplify Params - DO NOT EDIT
+	API_JCMOBILEAPI_APIID
+	API_JCMOBILEAPI_APINAME
+	API_JCMOBILE_GRAPHQLAPIENDPOINTOUTPUT
+	API_JCMOBILE_GRAPHQLAPIIDOUTPUT
+	AUTH_JCMOBILE_USERPOOLID
+	ENV
+	FUNCTION_JCMOBILECREATECOGNITOORGUSER_NAME
+	REGION
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  }, 
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
