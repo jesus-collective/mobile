eabd9ba3d0144acef44d18ccf5c7c7f4d32396bf
George Bell <47749730+GeorgeBellTMH@users.noreply.github.com>
2021-01-26 06:18:02 -0500

    Image updates

--- amplify/backend/function/S3Trigger14f1cefa/src/index.js
+++ amplify/backend/function/S3Trigger14f1cefa/src/index.js
@@ -1,20 +1,19 @@
 /* Amplify Params - DO NOT EDIT
 	ENV
 	REGION
-Amplify Params - DO NOT EDIT */const resizeHandler = require("./resizeHandler.js");
+Amplify Params - DO NOT EDIT */ const resizeHandler = require("./resizeHandler.js")
 
 exports.handler = async function (event, context) {
   //eslint-disable-line
-  console.log('141cefa - Received S3 event:', JSON.stringify(event, null, 2));
+  console.log("141cefa - Received S3 event:", JSON.stringify(event, null, 2))
   // Get the object from the event and show its content type
-  const bucket = event.Records[0].s3.bucket.name; //eslint-disable-line
-  const key = event.Records[0].s3.object.key; //eslint-disable-line
-  console.log(`Bucket: ${bucket}`, `Key: ${key}`);
+  const bucket = event.Records[0].s3.bucket.name //eslint-disable-line
+  const key = event.Records[0].s3.object.key //eslint-disable-line
+  console.log(`Bucket: ${bucket}`, `Key: ${key}`)
   try {
-    const imagePath = await resizeHandler.process(bucket, key);
+    const imagePath = await resizeHandler.process(bucket, key)
   } catch (error) {
-    console.log(error);
-
+    console.log(error)
   }
-  context.done(null, 'Successfully processed S3 event'); // SUCCESS with message
-};
+  context.done(null, "Successfully processed S3 event") // SUCCESS with message
+}
