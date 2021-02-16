4d4025b97a24b9fe51e5ee4bccc26b1025b6e615
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2021-02-15 23:12:52 -0500

    EditableUrl: fix race condition
    component keeps internal state for the URL value
    the onBlur prop triggers the mutation/context update

--- components/Forms/EditableUrl.tsx
+++ components/Forms/EditableUrl.tsx
@@ -1,6 +1,5 @@
-import { useNavigation, useRoute } from "@react-navigation/native"
 import React from "react"
-import { TextInput } from "react-native"
+import { StyleProp, TextInput, TextStyle } from "react-native"
 import JCButton, { ButtonTypes } from "../Forms/JCButton"
 import JCComponent from "../JCComponent/JCComponent"
 
@@ -9,54 +8,65 @@ interface Props {
   title: string
   isEditable: boolean
   textStyle: ButtonTypes
-  inputStyle?: any
+  inputStyle?: StyleProp<TextStyle>
   multiline: boolean
   placeholder?: string
-  onChange?(value: string): void
-  navigation?: any
-  route?: any
-}
-class EditableUrlImpl extends JCComponent<Props> {
-  onChanged(val: any) {
-    if (this.props.onChange) this.props.onChange(val.target.value)
-  }
-  navigate(id: string) {
-    // window.location.href = id
-    window.open(this.props.value, "_blank")
-  }
-  render() {
-    if (this.props.isEditable)
-      return (
-        <TextInput
-          onChange={(value) => {
-            this.onChanged(value)
-          }}
-          placeholder={this.props.placeholder}
-          multiline={this.props.multiline}
-          onStartShouldSetResponder={() => true}
-          onMoveShouldSetResponderCapture={() => true}
-          onStartShouldSetResponderCapture={() => true}
-          onMoveShouldSetResponder={() => true}
-          style={this.props.inputStyle}
-          value={this.props.value}
-        ></TextInput>
-      )
-    else
-      return this.props.value && this.props.value != "" ? (
-        <JCButton
-          buttonType={this.props.textStyle}
-          onPress={() => {
-            this.navigate(this.props.value)
-          }}
-        >
-          {this.props.title}
-        </JCButton>
-      ) : null
-  }
+  onChange(value: string): void
 }
 
-export default function EditableUrl(props: Props): JSX.Element {
-  const route = useRoute()
-  const navigation = useNavigation()
-  return <EditableUrlImpl {...props} navigation={navigation} route={route} />
+interface State {
+  url: string
+}
+
+export default class EditableUrlImpl extends JCComponent<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      url: props.value,
+    }
+  }
+
+  render(): JSX.Element | null {
+    const {
+      onChange,
+      isEditable,
+      placeholder,
+      multiline,
+      inputStyle,
+      textStyle,
+      value,
+      title,
+    } = this.props
+    const { url } = this.state
+
+    if (isEditable) {
+      return (
+        <TextInput
+          onChange={(e) => {
+            this.setState({ url: e.nativeEvent.text })
+          }}
+          onBlur={() => onChange(url)}
+          placeholder={placeholder}
+          multiline={multiline}
+          style={inputStyle}
+          value={url}
+        />
+      )
+    }
+
+    if (value) {
+      return (
+        <JCButton
+          buttonType={textStyle}
+          onPress={() => {
+            window.open(value, "_blank")
+          }}
+        >
+          {title}
+        </JCButton>
+      )
+    }
+
+    return null
+  }
 }
