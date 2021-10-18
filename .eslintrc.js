7b08bd0f8700e77b86a3668e3c80ec79a39efd6b
George Bell <47749730+GeorgeBellTMH@users.noreply.github.com>
2021-10-18 15:29:50 -0400

    aws-export changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,16 +7,16 @@ module.exports = {
       jsx: true, // Allows for the parsing of JSX
     },
   },
+  env: {
+    node: true,
+  },
   settings: {
     react: {
       version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
     },
   },
   extends: [
-    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
-    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "eslint:recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
