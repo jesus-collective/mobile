05f929782d5ca007254ee0fa8a7df17722652dda
Igor Belem <igortbelem@gmail.com>
2021-11-16 10:58:05 -0500

    Created EventsScreen Events fetch and display Event join/leave

--- screens/EventsScreen/EventsScreen.tsx
+++ screens/EventsScreen/EventsScreen.tsx
@@ -1,77 +1,152 @@
-﻿import { Route } from "@react-navigation/native"
+﻿import Auth from "@aws-amplify/auth"
+import { Route } from "@react-navigation/native"
 import { StackNavigationProp } from "@react-navigation/stack"
-import { Container, Content } from "native-base"
-import React from "react"
+import moment from "moment"
+import React, { useState } from "react"
+import { Text, View } from "react-native"
+import { JCCognitoUser } from "src/types"
+import { Data } from "../../components/Data/Data"
+import GenericButton from "../../components/FaceLift/GenericButton"
+import { GenericButtonStyles } from "../../components/FaceLift/GenericButtonStyles"
+import GenericContainer from "../../components/FaceLift/GenericContainer"
 import Header from "../../components/Header/Header"
-import JCComponent, { JCState } from "../../components/JCComponent/JCComponent"
-import MyGroups, { MapData } from "../../components/MyGroups/MyGroups"
-import MyMap from "../../components/MyMap/MyMap"
+import { GroupByTypeByTimeQueryVariables, ModelSortDirection } from "../../src/API"
+import EventsList from "./EventsList"
+import JCWidget from "./JCWidget"
 
 interface Props {
   navigation: StackNavigationProp<any, any>
   route: Route<any, { showAllEvents?: boolean; mine?: boolean }>
 }
-interface State extends JCState {
-  showMap: boolean
-  mapData: MapData[]
-}
 
-export default class HomeScreen extends JCComponent<Props, State> {
-  constructor(props: Props) {
-    super(props)
-    this.state = {
-      ...super.getInitialState(),
-      mapData: [],
-      showMap: false,
+export default function EventsScreen(props: Props) {
+  const { navigation } = props
+  const [reverse, setReverse] = useState(false)
+  const [filter, setFilter] = useState("")
+  const loadUpcoming = async () => {
+    const makeQueryVariables = (
+      nextToken: GroupByTypeByTimeQueryVariables["nextToken"],
+      past: boolean,
+      limit: number
+    ): GroupByTypeByTimeQueryVariables => {
+      return {
+        nextToken,
+        type: "event",
+        limit,
+        time: {
+          ge: moment().format(),
+        },
+        sortDirection: ModelSortDirection.ASC,
+      }
     }
+
+    const json = await Data.groupByTypeByTime(makeQueryVariables(null, false, 60))
+    const loadUser = async () => {
+      const jcUser: JCCognitoUser = await Auth.currentAuthenticatedUser()
+      return jcUser.username
+    }
+
+    const user = await loadUser()
+    const ownedEvents: Array<string> = []
+    const returnedEvents = json.data?.groupByTypeByTime?.items ?? []
+    console.log({ returnedEvents })
+    for (const item of returnedEvents) {
+      if (item?.ownerUser?.id === user) {
+        ownedEvents.push(item.id)
+      }
+      if (ownedEvents.length >= 2) break
+    }
+    return returnedEvents.filter((event) => ownedEvents.find((a) => a === event?.id))
   }
-  mapChanged = (): void => {
-    this.setState({ showMap: !this.state.showMap })
-  }
-  mergeMapData(mapData: MapData[]): void {
-    //    console.log(mapData)
-    const data = this.state.mapData.concat(mapData)
-    this.setState({ mapData: data })
-  }
-  render(): React.ReactNode {
-    console.log("EventsScreen")
+
+  const ControlButtons = () => {
     return (
-      <Container testID="events">
-        <Header
-          title="Jesus Collective"
-          navigation={this.props.navigation}
-          onMapChange={this.mapChanged}
-        />
-        <Content>
-          <MyMap
-            type={"no-filters"}
-            size={"50%"}
-            mapData={this.state.mapData}
-            visible={this.state.showMap}
-          ></MyMap>
-          <Container style={this.styles.style.eventsScreenMainContainer}>
-            <Container style={this.styles.style.eventsScreenLeftContainer}>
-              <MyGroups
-                showMy={!!this.props.route?.params?.mine}
-                showAllEvents={!!this.props.route?.params?.showAllEvents}
-                showMore={true}
-                type="event"
-                wrap={true}
-                navigation={this.props.navigation}
-                onDataload={(mapData: MapData[]) => {
-                  this.mergeMapData(mapData)
-                }}
-              ></MyGroups>
-            </Container>
-            {/*
-            <Container style={style.eventsScreenRightContainer}>
-              <MyPeople wrap={false} navigation={this.props.navigation} onDataload={(mapData) => { this.mergeMapData(mapData) }}></MyPeople>
-              <MyConversations navigation={this.props.navigation}> </MyConversations>
-              <Container ></Container>
-            </Container>*/}
-          </Container>
-        </Content>
-      </Container>
+      <View style={{ flexDirection: "row", justifyContent: "flex-end", marginBottom: 112 }}>
+        <GenericButton
+          label="SORT"
+          action={() => setReverse((prev) => !prev)}
+          style={{
+            ButtonStyle: GenericButtonStyles.SecondaryButtonStyle,
+            LabelStyle: GenericButtonStyles.SecondaryLabelStyle,
+            custom: {
+              marginRight: 32,
+            },
+          }}
+          icon="Sort"
+        ></GenericButton>
+        <GenericButton
+          label={`FILTER${filter ? ": My Events" : ""}`}
+          action={() => {
+            if (filter) setFilter("")
+            else setFilter(": Your Events")
+          }}
+          style={{
+            ButtonStyle: filter
+              ? GenericButtonStyles.PrimaryButtonStyle
+              : GenericButtonStyles.SecondaryButtonStyle,
+            LabelStyle: filter
+              ? GenericButtonStyles.PrimaryLabelStyle
+              : GenericButtonStyles.SecondaryLabelStyle,
+            custom: {
+              marginRight: 32,
+            },
+          }}
+          icon={filter ? "X" : "Filter"}
+        ></GenericButton>
+        <GenericButton
+          label="NEW EVENT"
+          action={() => null}
+          style={{
+            ButtonStyle: GenericButtonStyles.PrimaryButtonStyle,
+            LabelStyle: GenericButtonStyles.PrimaryLabelStyle,
+          }}
+          icon="Plus"
+        ></GenericButton>
+      </View>
     )
   }
+  return (
+    <View>
+      <Header title="Jesus Collective" navigation={navigation} />
+      <View style={{ marginHorizontal: "7.778vw" }}>
+        <Text
+          style={{
+            marginTop: 160,
+            marginBottom: 80,
+            fontFamily: "Graphik-Regular-App",
+            fontWeight: "400",
+            fontSize: 96,
+            lineHeight: 96,
+            letterSpacing: -0.5,
+            color: "#483938",
+          }}
+        >
+          Events
+        </Text>
+
+        <View
+          style={{
+            borderBottomColor: "#E4E1E1",
+            borderBottomWidth: 1,
+            marginBottom: 24,
+          }}
+        />
+        <ControlButtons />
+        <GenericContainer
+          MainContentComponent={() => <EventsList filter={filter} reverse={reverse}></EventsList>}
+          MinorContentComponent={() => (
+            <>
+              <JCWidget
+                emptyMessage="No upcoming events"
+                loadData={loadUpcoming}
+                title="Your Upcoming Events"
+              />
+            </>
+          )}
+          reverseContent
+          ToggleBarComponent={() => null}
+        />
+      </View>
+    </View>
+  )
 }
