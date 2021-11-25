996e0dd2db0c32e9d9b4b1d535efa1431b0c6c77
Igor Belem <igortbelem@gmail.com>
2021-11-24 20:03:04 -0500

    Improvements to Group Card UI

--- screens/GroupsScreen/GroupCard.tsx
+++ screens/GroupsScreen/GroupCard.tsx
@@ -7,20 +7,25 @@ const GroupCardStyle = StyleSheet.create({
   CardContainer: {
     border: "1px solid #E4E1E1",
     borderRadius: 8,
-    height: 384,
+    height: 308,
     flex: 1,
     marginBottom: 32,
     marginRight: 32,
     maxWidth: "calc(50% - 32px)",
   },
 })
-
-export default function GroupCard() {
+type Props = {
+  item: any
+}
+export default function GroupCard(props: Props) {
+  const { item } = props
+  const { name, description, owner, ownerUser } = item
   return (
-    <Tooltip title={"group nmae"}>
+    <Tooltip title={name}>
       <TouchableOpacity style={GroupCardStyle.CardContainer}>
         <View style={{ padding: 16, flex: 1 }}>
           <Text
+            numberOfLines={3}
             style={{
               fontSize: 15,
               fontFamily: "Graphik-Regular-App",
@@ -30,68 +35,85 @@ export default function GroupCard() {
               paddingBottom: 2,
             }}
           >
-            Name of Group
+            {name}
           </Text>
+          {item.location ? (
+            <Text
+              numberOfLines={1}
+              style={{
+                fontSize: 15,
+                fontFamily: "Graphik-Regular-App",
+                fontWeight: "400",
+                lineHeight: 24,
+                paddingBottom: 16,
+                color: "#6A5E5D",
+              }}
+            >
+              {item.location}
+            </Text>
+          ) : null}
           <Text
+            numberOfLines={4}
             style={{
               fontSize: 15,
               fontFamily: "Graphik-Regular-App",
               fontWeight: "400",
               lineHeight: 24,
-              paddingBottom: 16,
-              color: "#6A5E5D",
-            }}
-          >
-            Location or some other metadata
-          </Text>
-          <Text
-            style={{
-              fontSize: 15,
-              fontFamily: "Graphik-Regular-App",
-              fontWeight: "400",
-              lineHeight: 24,
-              paddingBottom: 32,
+
               color: "#1A0706",
             }}
           >
-            Began hair I that caches roasted clock eventually. He step the both of he about she
-            question. Hand, and even and at avoid to her is have taking her salesman remember
-            multitude her you amped intended to before explorations do.
+            {description} {description} {description} {description} {description} {description}{" "}
+            {description} {description} {description} {description}
           </Text>
           <View
             style={{
               flexDirection: "row",
               justifyContent: "flex-start",
-              alignItems: "center",
+              alignItems: "flex-end",
+              flex: 1,
+              paddingTop: 32,
             }}
           >
-            <View style={{ flexDirection: "column", marginLeft: -10 }}>
-              <ProfileImage size="small2" user={""} />
-            </View>
-            <View style={{ flexDirection: "column" }}>
-              <Text
-                style={{
-                  color: "#6A5E5D",
-                  fontWeight: "600",
-                  fontSize: 12,
-                  letterSpacing: 1,
-                  lineHeight: 16,
-                  fontFamily: "Graphik-Regular-App",
-                  textTransform: "uppercase",
-                }}
-              >
-                Organizer
-              </Text>
-              <Text
-                style={{
-                  fontFamily: "Graphik-Regular-App",
-                  color: "#1a0706",
-                  lineHeight: 24,
-                  fontSize: 15,
-                }}
-              >
-                First Name Last Name
-              </Text>
+            <View
+              style={{
+                alignItems: "center",
+                flex: 1,
+                flexDirection: "row",
+              }}
+            >
+              <View style={{ flexDirection: "column", marginLeft: -10 }}>
+                <ProfileImage size="small2" user={owner} />
+              </View>
+              <View style={{ flexDirection: "column", flex: 1 }}>
+                <Text
+                  numberOfLines={1}
+                  style={{
+                    color: "#6A5E5D",
+                    fontWeight: "600",
+                    fontSize: 12,
+                    letterSpacing: 1,
+                    lineHeight: 16,
+                    flex: 1,
+                    fontFamily: "Graphik-Regular-App",
+                    textTransform: "uppercase",
+                  }}
+                >
+                  Organizer
+                </Text>
+                <Text
+                  numberOfLines={3}
+                  style={{
+                    fontFamily: "Graphik-Regular-App",
+                    color: "#1a0706",
+                    lineHeight: 24,
+                    flex: 1,
+                    fontSize: 15,
+                  }}
+                >
+                  {ownerUser?.given_name ?? ""} {ownerUser?.family_name ?? ""}
+                </Text>
+              </View>
             </View>
           </View>
         </View>
